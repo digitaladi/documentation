5d81d6cd7d591e64e872c90a1a591975759d98a8
digitaladi <aladi.timera1605@gmail.com>
2019-05-12 23:22:30 +0200

    1 er commit

---
+++ docu_ctcvert.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <title>Documentation CtcVert</title>
+</head>
+<body>
+
+<h1>Documentation de l'application CtcVert</h1>
+
+<!------ il fait hr ---------->
+
+<!--<div id="hr"></div>-->
+<!--<img src="imges/fleuriste-2.jpg" alt="">-->
+<table>
+    <thead>
+    <tr>
+        <th>Date</th>
+        <th>auteur</th>
+        <th>titre</th>
+        <th>Descriptif</th>
+        <th>Langage ou outils</th>
+        <th>commentaires</th>
+        <th>liens</th>
+    </tr>
+    </thead>
+
+    <tbody>
+    <tr>
+        <td>12/05/2019</td>
+        <td>Aladi</td>
+        <td>hr déguisé</td>
+        <td>Le principe est de tranformer un div en hr en mettant un border-top avec comme valeur : <u>1px solid #efefef</u>. Ainsi le trait est plus fin</td>
+        <td>CSS ET HTML</td>
+        <td>En fait un hr normal aura toujours un border plus appaisse. Voila pourquoi on a utilisé ce cas</td>
+        <td></td>
+    </tr>
+    </tbody>
+
+</table>
+
+</body>
+</html>
